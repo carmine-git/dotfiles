566f010bf28978f584c5494d90fc6185ae4549d0
carmine-git <amaryllismain09@gmail.com>
2023-09-28 13:56:23 +0200

    ❌ Nvim folder removed

--- backup/nvim/lua/plugins/formatters.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"mhartington/formatter.nvim",
-	config = function()
-		local config = require("configs.formatters")
-		config.keybinds()
-		config.formatter()
-	end,
-}
--- backup/nvim/lua/plugins/lsp.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"neovim/nvim-lspconfig",
-	name = "lspconfig",
-	config = function()
-		require('configs.lsp').lsp_config()
-	end,
-}
