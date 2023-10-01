5f5483cbf60b23ac3e5ba19744fc907883b99b95
carmine-git <amaryllismain09@gmail.com>
2023-10-01 16:04:15 +0200

    Chore(file): Add prettierrc.mjs

---
+++ prettierrc.mjs
@@ -0,0 +1,11 @@
+const config = {
+  trailingComma: "all",
+  arrowParens: "always",
+  singleQuote: true,
+  semi: true,
+  useTabs: true,
+  tabWidth: 2,
+  printWidth: 80,
+};
+
+export default config;
